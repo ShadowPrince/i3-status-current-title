54d5cd3af768ab757a0f99bf49132349df61a1ef
ShadowPrince <shadowprince47@gmail.com>
2013-11-08 18:32:54 +0200

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+## Overview
+Puts current window's title into status bar at position 0, if there is no active window puts there current workspace name.
+
+## Installation
+First of all you need [py3status](http://github.com/ultrabug/py3status) installed and configured, than you copy i3-status-current-title.py inside your's plugins directory.
+
+## Configuration
+There is some constants located at beginning of the file:
+    * `MAX_WIDTH`: if length of title greater than that value it will be cutted to last 60 chars (not first, but last).
+    * `CACHED_TIME`: cached time in seconds, delay in which information about current window updates. Default set to 0, so information updates as fast as py3status updates 
---
+++ i3-status-current-title.py
@@ -0,0 +1,35 @@
+import i3
+import time
+
+MAX_WIDTH = 80
+CACHED_TIME = 0
+
+def find_focused(tree):
+    if type(tree) == list:
+        for el in tree:
+            res = find_focused(el)
+            if res:
+                return res
+
+    elif type(tree) == dict:
+        if tree["focused"]:
+            return tree
+        else:
+            return find_focused(tree["nodes"] + tree["floating_nodes"])
+
+class Py3status:
+    def currentTitle(self, json, i3status_config):
+        response = {'full_text': '', 'name': 'current-title', 'cached_until': time.time() + CACHED_TIME}
+
+        try:
+            window = find_focused(i3.get_tree())
+
+            if window and "name" in window:
+                response["full_text"] = len(window["name"]) > MAX_WIDTH and "..." + window["name"][-MAX_WIDTH:-1] or window["name"]
+
+            # There is such encode in py3status, if it fails plugin will shutdown
+            response['full_text'].encode('utf-8')
+        except:
+            pass
+
+        return (0, response)
